4bd5a69485f2b8e59098d9ea53b96f9c7e029b0d
deevolution <deevolutionism@gmail.com>
2019-01-29 16:25:31 -0500

    add endpoint for getting data from user input address or zip

--- server/index.js
+++ server/index.js
@@ -4,10 +4,13 @@ const app = express()
 const http = require('http').Server(app)
 const path = require('path')
 const port = process.env.PORT || 3000
-const query
 const fetch = require('node-fetch')
-const DARKSKY_KEY = process.env["DARKSKYKEY"]
+const DARKSKY_KEY = process.env["DARKSKY_KEY"]
 const GOOGLE_GEOCODE_KEY = process.env["GOOGLE_GEOCODE_KEY"]
+
+if( process.env.NODE_ENV == "dev" ) {
+
+}
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded( {extended: true} ))
@@ -15,17 +18,31 @@ app.use(express.static(path.join(__dirname, '../dist')))
 http.listen(port, () => console.log(`listening on port ${port}`))
 
 const darksky_base = `https://api.darksky.net/forecast/${DARKSKY_KEY}`
-const geocode_base = `https://maps.googleapis.com/maps/api/geocode/json?key=${GEOCODE_KEY}&sensor=true`
+const geocode_base = `https://maps.googleapis.com/maps/api/geocode/json?key=${GOOGLE_GEOCODE_KEY}&sensor=true`
 
 app.get('/api/darksky', async ( req, res ) => {
     console.log(req.body)
-    
-    let response = await fetch(`${darksky_base}/${body.lat},${body.lon}?units=${body.unit}`)
+    let { lat, lon } = req.body
+    let response = await fetch(`${darksky_base}/${lat},${lon}?units=${body.unit}`)
     let json = await response.json()
     res.json(json)
 })
 
 app.get('/api/geocode', async (req, res) => {
-    let { lat, lon } = req.body
-    let response = await fetch(`${geocode_base}&latlng=${lat},${lon}`)
+    let { lat, lon } = req.query
+    
+    console.log(lat, lon)
+    let uri = `${geocode_base}&latlng=${lat},${lon}`
+    
+    console.log(uri)
+    // let response = await fetch(`${geocode_base}&latlng=${lat},${lon}`)
+    // let json = await response.json()
+    // res.json(json)
+    res.json({lat, lon})
+})
+
+app.get('/test', async (req, res) => {
+    let response = await fetch("https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452&sensor=true&key=AIzaSyAM31b3gxNR8cQ6DG6Q5VtkgEoNJiudESI")
+    let json = await response.json()
+    res.json(json)
 })
